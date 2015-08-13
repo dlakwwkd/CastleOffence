2de609afa3e126ca19bdb0a036edc7e466b8eaa4
Subin Park <dlakek@naver.com>
2015-08-14 03:34:56 +0900

    BarrierContainer 켜고 끄는 버튼 추가

---
+++ CastleOffence/Assets/Script/ContainerOnOff.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContainerOnOff : MonoBehaviour
+{
+    UIWidget    _widget = null;
+    bool        _isOn   = false;
+
+    void Start()
+    {
+        _widget = GetComponent<UIWidget>();
+        _widget.alpha = 0.0f;
+    }
+
+    public void Click()
+    {
+        if(_isOn)
+        {
+            _widget.alpha = 0.0f;
+            _isOn = false;
+        }
+        else
+        {
+            _widget.alpha = 1.0f;
+            _isOn = true;
+        }
+    }
+}
